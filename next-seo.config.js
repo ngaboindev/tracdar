0368d3524bf98f136d29b4e7c326783e7e813b36
Robert Ngabo <robertngabo2002@gmail.com>
2023-06-01 12:11:15 +0200

    first commit

---
+++ next-seo.config.js
@@ -0,0 +1,26 @@
+/** @type {import('next-seo').DefaultSeoProps} */
+const defaultSEOConfig = {
+  title: "Tracdar",
+  titleTemplate: "%s | tracdar",
+  defaultTitle: "tracdar",
+  description: "Take control of your finances with our subscription expense tracker. Easily monitor, manage, and optimize your subscriptions all in one place. Start saving money today!",
+  canonical: "",
+  openGraph: {
+    url: "",
+    title: "nextarter-chakra",
+    description: "Take control of your finances with our subscription expense tracker. Easily monitor, manage, and optimize your subscriptions all in one place. Start saving money today!",
+    images: [
+      {
+        url: "",
+        alt: "nextarter-chakra.sznm.dev og-image",
+      },
+    ],
+    site_name: "tracdar",
+  },
+  twitter: {
+    handle: "@ngabo",
+    cardType: "summary_large_image",
+  },
+};
+
+export default defaultSEOConfig;
